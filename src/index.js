97fc14467e07db8c5b4d0ef3ec6523c0ac16eb71
June-Junior <artknownuntist@gmail.com>
2020-12-08 05:42:00 +0200

    promise2of3

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-// Task 3.1
+// ______________________________________Task 3.1__________________________________________________________________________
 // Напиши функцию delay(ms), которая возвращает промис, переходящий в состояние "resolved" через ms миллисекунд. 
 // Значением исполнившегося промиса должно быть то кол-во миллисекунд которое передали во время вызова функции delay.
 
@@ -36,3 +36,41 @@ const loggerPlus = timePlus => console.log(`Resolved after ${timePlus}ms`);
 delayPlus(2500).then(loggerPlus); 
 delayPlus(3000).then(loggerPlus); 
 delayPlus(3500).then(loggerPlus);
+
+
+// _____________________________________Task 3.2_________________________________________________________________________
+
+// Перепиши функцию toggleUserState() так, чтобы она не использовала callback-функцию callback, а принимала всего два параметра allUsers и userName и возвращала промис.
+
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+  return new Promise((resolve, reject) => {
+      const updatedUsers = allUsers.map(user =>
+      user.name === userName ? { ...user, active: !user.active } : user,
+    );
+  
+      resolve(updatedUsers);
+  })
+  
+};
+
+const loggerSecond = updatedUsers => console.table(updatedUsers);
+
+/*
+ * Сейчас работает так
+ */
+// toggleUserState(users, 'Mango', loggerSecond);
+// toggleUserState(users, 'Lux', loggerSecond);
+
+/*
+ * Должно работать так
+ */
+toggleUserState(users, 'Mango').then(loggerSecond);
+toggleUserState(users, 'Lux').then(loggerSecond);
+
