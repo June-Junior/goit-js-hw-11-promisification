cbfe35d2cc943decf81c9d1924b0c683ae598fb0
June-Junior <artknownuntist@gmail.com>
2020-12-08 06:02:28 +0200

    commit-task3of3

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@
 // Напиши функцию delay(ms), которая возвращает промис, переходящий в состояние "resolved" через ms миллисекунд. 
 // Значением исполнившегося промиса должно быть то кол-во миллисекунд которое передали во время вызова функции delay.
 
+// const { resolve, reject } = require("core-js/fn/promise"); - откуда это постоянно берется???
+
 const delay = ms => {
   // Твой код
   const pause = ms;
@@ -74,3 +76,63 @@ const loggerSecond = updatedUsers => console.table(updatedUsers);
 toggleUserState(users, 'Mango').then(loggerSecond);
 toggleUserState(users, 'Lux').then(loggerSecond);
 
+
+// _____________________________________Task 3.3_________________________________________________________________________
+
+// Перепиши функцию makeTransaction() так, чтобы она не использовала callback-функции onSuccess и onError, 
+// а принимала всего один параметр transaction и возвращала промис.
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const makeTransaction = (transaction, onSuccess, onError) => {
+  const delay = randomIntegerFromInterval(200, 500);
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const canProcess = Math.random() > 0.3;
+  
+      if (canProcess) {
+        resolve(transaction.id, delay);
+      } 
+        reject(transaction.id);
+      
+    }, delay);
+  });
+  
+};
+
+const logSuccess = (id, time) => {
+  console.log(`Transaction ${id} processed in ${time}ms`);
+};
+
+const logError = id => {
+  console.warn(`Error processing transaction ${id}. Please try again later.`);
+};
+
+/*
+ * Работает так
+ */
+// makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
+// makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
+// makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
+// makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
+/*
+ * Должно работать так
+ */
+makeTransaction({ id: 70, amount: 150 })
+  .then(logSuccess)
+  .catch(logError);
+
+makeTransaction({ id: 71, amount: 230 })
+  .then(logSuccess)
+  .catch(logError);
+
+makeTransaction({ id: 72, amount: 75 })
+  .then(logSuccess)
+  .catch(logError);
+
+makeTransaction({ id: 73, amount: 100 })
+  .then(logSuccess)
+  .catch(logError);
